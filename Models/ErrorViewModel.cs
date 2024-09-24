cd75e8b3510e522a76a87dce1711ee43a9788045
Ömer Doğan <omerdogan@ankara.turkmedya.local>
2024-09-24 15:26:14 +0300

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace goldozonmedical.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
