482182c1b6f40be3f53028af7395c6b4e879bd06
Ömer Doğan <omerdogan@ankara.turkmedya.local>
2024-09-25 17:26:41 +0300

    Update layout and footer

--- wwwroot/js/layout.js
+++ wwwroot/js/layout.js
@@ -1,31 +1,39 @@
 
 var hamburgerButton = document.getElementById("hamburgerbutton")
-
+var ham = document.querySelector(".hamburgercontainer")
 
 
 
 function toggleMenu() {
-    var ham = document.querySelector(".hamburgercontainer")
+    
     var xKonumu = ham.getBoundingClientRect().left;
     if(xKonumu<= -250){
         
         ham.style.transform = 'translate(249px,0px)';  // 250px sağa
-        ham.style.transition = 'transform 0.8s ease-out';  // Yumuşak geçiş için transition
+        ham.style.transition = 'transform 0.6s ease-out';  // Yumuşak geçiş için transition
+        hamburgerButton.style.transform = 'rotate(-90deg)'
+        hamburgerButton.style.transition = 'transform 0.6s ease'; 
     }
     else{
         ham.style.transform = 'translate(-249px, 0px)';  // 250px sola
-        ham.style.transition = 'transform 0.8s ease-in';  // Yumuşak geçiş için transition
+        ham.style.transition = 'transform 0.6s ease-in';  // Yumuşak geçiş için transition
+        hamburgerButton.style.transform = 'rotate(0deg)'
+        hamburgerButton.style.transition = 'transform 0.6s ease'; 
     }
     
 }
 
 // Menü dışına tıklanıldığında kapat
 document.addEventListener('click', function (event) {
-    var ham = document.querySelector(".hamburgercontainer")
+    
+    var xKonumu = ham.getBoundingClientRect().left;
     // Eğer tıklanan alan hamburger menüsü veya menü tuşu değilse menüyü kapat
-    if (!ham.contains(event.target) && !hamburgerButton.contains(event.target)) {
-        toggleMenu();
+    if(xKonumu > -250){
+        if (!ham.contains(event.target) && !hamburgerButton.contains(event.target)) {
+            toggleMenu();
+        }
     }
+    
 });
 
 
