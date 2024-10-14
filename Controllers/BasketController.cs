04a22ab7b586a03500a0d9537224defd3668ff0c
emre bakımcı <115481070+emrebakimci@users.noreply.github.com>
2024-10-14 18:57:47 +0300

    Basket system

---
+++ Controllers/BasketController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using goldozonmedical_Web_Site.Models;
+using System.Collections.Generic;
+
+public class BasketController : Controller
+{
+    
+    private static List<ProductPrice> cart = new List<ProductPrice>();
+
+   
+    public ActionResult AddToCart(int id)
+    {
+        var product = GetProductById(id); 
+        if (product != null)
+        {
+            cart.Add(product);
+        }
+        return RedirectToAction("Index", "PriceProduct");
+    }
+
+  
+    public ActionResult Buy(int id)
+    {
+        var product = GetProductById(id);
+        if (product != null)
+        {
+            cart.Add(product);
+        }
+        return RedirectToAction("Basket");
+    }
+
+    public ActionResult Basket()
+    {
+        return View(cart);
+    }
+
+    
+    public ActionResult RemoveFromCart(int id)
+    {
+        var product = cart.Find(p => p.Id == id);
+        if (product != null)
+        {
+            cart.Remove(product);
+        }
+        return RedirectToAction("Basket");
+    }
+
+   
+    private ProductPrice GetProductById(int id)
+    {
+        var products = new List<ProductPrice>
+        {
+            new ProductPrice { Id = 1, Name = "Ürün 1", ImageUrl = "~/img/maymun.jpg", Price = 100m, DiscountedPrice = 80m },
+            new ProductPrice { Id = 2, Name = "Ürün 2", ImageUrl = "~/img/maymun.jpg", Price = 150m, DiscountedPrice = 120m },
+            new ProductPrice { Id = 3, Name = "Ürün 3", ImageUrl = "~/img/maymun.jpg", Price = 200m, DiscountedPrice = 180m }
+        };
+
+        return products.Find(p => p.Id == id);
+    }
+}
